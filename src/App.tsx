b3e7b27ae5ba8ca8be974ba60abeb0c538c06b37
Priyanshu Kumar <136223448+PriyanshuKumar9798@users.noreply.github.com>
2025-10-05 22:42:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Layout from "./components/Layout";
 import Layout from "./pages/Layout"
 import Index from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import DiscoverStartup from "./pages/DiscoverStartup";
 import StartupProfile from "./pages/StartupProfile";
 import Preorder from "./pages/Preorder";
+// import SignIn from "./pages/SignIn";
+// import Signin from "./pages/SignIn";
+import SignIn from "./pages/Signin";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
 
 const queryClient = new QueryClient();
 
@@ -25,6 +29,9 @@ const App = () => (
             <Route path="/discover-startup" element={<DiscoverStartup />} />
             <Route path="/startup-profile" element={<StartupProfile />} />
             <Route path="/preorder" element={<Preorder />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
 
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
