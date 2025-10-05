b6d85e3e4a559499d4fc5187ec6025f8d00e4e11
Priyanshu Kumar <136223448+PriyanshuKumar9798@users.noreply.github.com>
2025-10-06 01:08:26 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout"
-import Index from "./pages/Home";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import DiscoverStartup from "./pages/DiscoverStartup";
 import StartupProfile from "./pages/StartupProfile";
 import Preorder from "./pages/Preorder";
 // import SignIn from "./pages/SignIn";
 // import Signin from "./pages/SignIn";
-import SignIn from "./pages/SignIn";
+import SignIn from "./pages/Signin";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import OurTeamPage from "./pages/OurTeamPage";
+import FaqPage from "./pages/FaqPage";
 
 const queryClient = new QueryClient();
 
@@ -25,13 +27,15 @@ const App = () => (
       <BrowserRouter>
         <Layout>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<Home />} />
             <Route path="/discover-startup" element={<DiscoverStartup />} />
             <Route path="/startup-profile" element={<StartupProfile />} />
             <Route path="/preorder" element={<Preorder />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/OurTeam" element={<OurTeamPage />} />
+            <Route path="/FaqPage" element={<FaqPage/>}/>
 
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
@@ -43,4 +47,3 @@ const App = () => (
 );
 
 export default App;
-
